6f88d0a4908d6ad68a6fbd34bbb656622e5e0a7a
FSzukalski <fszukalski@gmail.com>
2023-09-11 10:43:46 +0200

    ShelterDTO and TempHouseDTO

---
+++ PetAdoptionCenter/DTOs/TempHouseDTO.cs
@@ -0,0 +1,15 @@
+﻿using PetAdoptionCenter.Models.Animal;
+using PetAdoptionCenter.Models.PetShelter;
+using PetAdoptionCenter.Models.WebUser;
+
+namespace PetAdoptionCenter.DTOs
+{
+    public class TempHouseDTO
+    {
+        public uint Id { get; init; }
+        public UserDTO TemporaryOwner { get; set; }
+        public Pet PetInTemporaryHouse { get; set; }
+        public ShelterDTO ShelterName { get; set; }
+        public DateTime StartOfTemporaryHouseDate { get; init; }
+    }
+}
