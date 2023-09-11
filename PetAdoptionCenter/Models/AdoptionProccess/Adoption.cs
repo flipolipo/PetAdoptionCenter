1984278debbd02fb4d63b1597e37c7d6b0d5e656
Barbara Smaga <barbara.smaga98@gmail.com>
2023-09-11 10:27:57 +0200

    Added class dtos

--- PetAdoptionCenter/Models/AdoptionProccess/Adoption.cs
+++ PetAdoptionCenter/Models/AdoptionProccess/Adoption.cs
@@ -1,14 +1,20 @@
 ﻿using PetAdoptionCenter.Models.Animal;
 using PetAdoptionCenter.Models.PetShelter;
 using PetAdoptionCenter.Models.WebUser;
+using System.ComponentModel.DataAnnotations;
 
 namespace PetAdoptionCenter.Models.AdoptionProccess;
 
 public class Adoption
 {
+    [Key]
     public uint Id { get; set; }
+    [Required]
     public Pet PetToAdoption { get; set; }
+    [Required]
     public Shelter Shelter { get; set; }
+    [Required]
     public User Adopter { get; set; }
+    [Required]
     public DateTime DateOfAdoption { get; set; }
 }
