6c66975e61e2b5df880727e65dc6714ec24bad7f
flipolipo <juroszekfilip123@gmail.com>
2023-09-13 14:16:12 +0200

    moved Repository to SimpleWebDal

--- SimpleWebDal/Repository/ShelterRepo/IShelterRepository.cs
+++ SimpleWebDal/Repository/ShelterRepo/IShelterRepository.cs
@@ -1,5 +1,6 @@
 ﻿using SimpleWebDal.Models.Animal;
 using SimpleWebDal.Models.PetShelter;
+using SimpleWebDal.Models.TemporaryHouse;
 using SimpleWebDal.Models.WebUser;
 using System;
 using System.Collections.Generic;
@@ -23,7 +24,12 @@ namespace SImpleWebLogic.Repository.ShelterRepo
         public Task<IEnumerable<User>> GetShelterWorkers(int shelterId);
         public Task<IEnumerable<User>> GetShelterContributors(int shelterId);
         public Task<IEnumerable<User>> GetShelterWorkersById(int shelterId, int workerId);
-
+        public Task<IEnumerable<User>> GetShelterContributorsById(int shelterId, int workerId);
+        public Task<IEnumerable<Pet>> GetAllAdoptedPets(int shelterId);
+        public Task<Pet> GetAdoptedPetsById(int shelterId, int petId);
+        public Task<TempHouse> GetTempHouseById(int shelterId,int tempHouseId);
+        public Task<IEnumerable<TempHouse>> GetAllTempHouses(int shelterId);
+        public Task<IEnumerable<Pet>> GetAll
 
 
 
