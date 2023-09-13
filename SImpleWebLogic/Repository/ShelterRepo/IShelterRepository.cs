80b95a87e485ca4e756c6b579b72bf229fec5dcd
flipolipo <juroszekfilip123@gmail.com>
2023-09-13 13:51:28 +0200

    Added endpoint folders

--- SImpleWebLogic/Repository/ShelterRepo/IShelterRepository.cs
+++ SImpleWebLogic/Repository/ShelterRepo/IShelterRepository.cs
@@ -1,12 +1,31 @@
-﻿using System;
+﻿using SimpleWebDal.Models.Animal;
+using SimpleWebDal.Models.PetShelter;
+using SimpleWebDal.Models.WebUser;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SImpleWebLogic.Repository.Shelter
+namespace SImpleWebLogic.Repository.ShelterRepo
 {
-    internal interface IShelterRepository
+    public interface IShelterRepository
     {
+        //Get
+        public bool SaveChange();
+        public Task<IEnumerable<Shelter>> GetAllShelters();
+        public Task<Shelter> GetShelterById(int shelterId); 
+        public void UpdateShelter(Shelter shelter);
+        public void DeleteShelter(int shelterId);
+        public Task<IEnumerable<Pet>> GetAllShelterPets(int shelterId);
+        public Task<IEnumerable<Pet>> GetAllShelterDogs(int shelterId);
+        public Task<Pet> GetShelterPetById(int shelterId, int petId);
+        public Task<IEnumerable<User>> GetShelterWorkers(int shelterId);
+        public Task<IEnumerable<User>> GetShelterContributors(int shelterId);
+        public Task<IEnumerable<User>> GetShelterWorkersById(int shelterId, int workerId);
+
+
+
+
     }
 }
