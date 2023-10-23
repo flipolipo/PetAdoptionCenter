382643526d8f2132d67848082fdaa493d7476d35
FSzukalski <fszukalski@gmail.com>
2023-10-23 11:01:09 +0200

    errors after merge resolved

--- pet-adoption-center-frontend/src/Pages/Shelters/Shelters.js
+++ pet-adoption-center-frontend/src/Pages/Shelters/Shelters.js
@@ -7,42 +7,41 @@ const Shelters = () => {
   const [shelters, setShelters] = useState([]);
   const [shelterChosen, setShelterChosen] = useState(false)
   const [shelterData, setShelterData] = useState({})
-const fetchShelters = async () => {
-  const response = await axios.get( 'https://localhost:7292/Shelters?_limit=3');
-  setShelters(response.data);
-  console.log(response.data)
-};
+  const fetchShelters = async () => {
+    const response = await axios.get(`${address_url}/Shelters?_limit=3`);
+    setShelters(response.data);
+    console.log(response.data)
+  };
 
-async function ClickHandler(id)  
-{
-  const response = await axios.get(`https://localhost:7292/Shelters/${id}`)
-setShelterChosen(true)
-setShelterData(response.data)
-}
-useEffect(() => {
-  fetchShelters();
-}, []);
+  async function ClickHandler(id) {
+    const response = await axios.get(`${address_url}/Shelters/${id}`)
+    setShelterChosen(true)
+    setShelterData(response.data)
+  }
+  useEffect(() => {
+    fetchShelters();
+  }, []);
   return (
     <div>
       {
-        shelterChosen ? 
-        (<div>{shelterData.Id}</div>) : 
-    (<div className='sheltersContainer'>
-        {shelters.map((shelter) => (
-          
-          <div className="shelterCard" onClick={() => ClickHandler(shelter.Id)}>
-            <img
-                src={`data:image/jpeg;base64, ${shelter.ImageBase64}`}
-                alt=""
-                width="250px"
-                height="250px"
-              />
-            <h4 className='textContainer'>{shelter.Name}</h4>
-          </div>
-         
-        ))}
-    </div>)
-}
+        shelterChosen ?
+          (<div>{shelterData.Id}</div>) :
+          (<div className='sheltersContainer'>
+            {shelters.map((shelter) => (
+
+              <div className="shelterCard" onClick={() => ClickHandler(shelter.Id)}>
+                <img
+                  src={`data:image/jpeg;base64, ${shelter.ImageBase64}`}
+                  alt=""
+                  width="250px"
+                  height="250px"
+                />
+                <h4 className='textContainer'>{shelter.Name}</h4>
+              </div>
+
+            ))}
+          </div>)
+      }
     </div>
   );
 };
