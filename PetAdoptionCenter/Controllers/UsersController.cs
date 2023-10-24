f69ac7ec98aef7662007fded1fb3df77b74da7ea
FSzukalski <fszukalski@gmail.com>
2023-10-24 23:59:41 +0200

    User Calendar, Pets, Adoption, Roles added Profile

--- PetAdoptionCenter/Controllers/UsersController.cs
+++ PetAdoptionCenter/Controllers/UsersController.cs
@@ -54,7 +54,7 @@ public class UsersController : ControllerBase
         var user = await _userRepository.GetUserById(id);
         if (user != null)
         {
-            return Ok(_mapper.Map<UserReadDTO>(user));
+            return Ok(user);
         }
         return NotFound();
     }
--- pet-adoption-center-frontend/src/Pages/Profile/Profile.js
+++ pet-adoption-center-frontend/src/Pages/Profile/Profile.js
@@ -2,12 +2,15 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useUser } from '../../Components/UserContext';
 import { address_url } from '../../Service/url';
+import UserRoleName from '../../Components/Enum/UserRoleName';
+import MyCalendar from '../../Components/BigCalendarActivity/CalendarActivity';
 const Profile = () => {
 
     const [profileData, setProfileData] = useState(null);
     const [isLoading, setIsLoading] = useState(true);
     const [error, setError] = useState(null);
     const { user } = useUser();
+
 
     useEffect(() => {
         const fetchProfileData = async () => {
@@ -32,6 +35,7 @@ const Profile = () => {
     if (isLoading) return <div>Loading...</div>;
     if (error) return <div>Error: {error}</div>;
 
+
     return (
         <div className="profile-container">
             <div className="header">
@@ -54,7 +58,39 @@ const Profile = () => {
                     </div>
                 ))}
             </div>
-            {/* Add more sections as needed */}
+            <div className="pets">
+                <h2>Your Pets</h2>
+                {profileData.Pets?.map(pet => (
+                    <div key={pet.Id} className="pet-card">
+                        <p>Pet ID: {pet.Id}</p>
+                        <p>Status: {pet.AvailbleForAdoption ? 'Availble for adoption' : 'Not availble for adoption'}</p>
+                        <p>Discription: {pet.Description}</p>
+                        <p>{pet.ImageBase64}</p>
+                    </div>
+                ))}
+            </div>
+            <div className="roles">
+                <h2>Your Roles</h2>
+                {profileData.Roles?.map(role => (
+                    <div key={role.Id} className="role-card">
+                        <p>Role ID: {role.Id}</p>
+                        <p>Role title: {UserRoleName(role.Title)}</p>
+                    </div>
+                ))}
+            </div>
+            <div className="calendar">
+                <h2>Your Calendar</h2>
+                <p>Calendar Id: {profileData.UserCalendar.Id}</p>
+                {profileData.UserCalendar.Activities?.map(activity => (
+                    <div key={activity.Id} className="calendar-card">
+                        <p>Activity ID: {activity.Id}</p>
+                        <p>Activity Name: {activity.Name}</p>
+                        <p>Activity Start: {activity.StartActivityDate}</p>
+                        <p>Activity End: {activity.EndActivityDate}</p>
+                    </div>
+                ))}
+            </div>
+            <MyCalendar events={profileData.UserCalendar.Activities} />
         </div>
     );
 };
