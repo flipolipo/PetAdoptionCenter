b8a543b0102af60e327a514ce0a7e218bbd3eff2
flipolipo <juroszekfilip123@gmail.com>
2023-12-01 16:09:05 +0100

    commented need account

--- pet-adoption-center-frontend/src/Components/Login.js
+++ pet-adoption-center-frontend/src/Components/Login.js
@@ -120,8 +120,8 @@ const Login = () => {
               />
               <button className="buttonLogin" onClick={loginUser}>Login</button>
               <button className="buttonLogin" onClick={() => setVisible(false)}>Back</button>
-              <h5 className='need-an-account'>Need an account?</h5>
-              <Link to={`/user/register`} onClick={() => setVisible(false)}>Sign Up</Link>
+              {/* <h5 className='need-an-account'>Need an account?</h5>
+              <Link to={`/user/register`} onClick={() => setVisible(false)}>Sign Up</Link> */}
             </div>
           </Modal>
         </>
